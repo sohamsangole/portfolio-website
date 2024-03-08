67719e96c05e422a85fd605e0b769d6a266d2244
Soham Sangole <sohamajaysangole@gmail.com>
2024-03-08 19:11:52 +0530

    Updated UI

--- src/app/about/slideshow.js
+++ src/app/about/slideshow.js
@@ -9,37 +9,39 @@ import { Image } from 'react-bootstrap';
 
 const SlideShow = () => {
     return (
-        <Swiper
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
-            spaceBetween={50}
-            slidesPerView={1}
-            navigation
-            pagination={{ clickable: true }}
-            scrollbar={{ draggable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log('slide change')}
-        >
-            <SwiperSlide>
-                <div className='flex items-center justify-center'>
-                    <Image src="/assets/vjti1.jpg" alt="Slide 1" />
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className='flex items-center justify-center'>
-                    <Image src="/assets/vjti2.jpg" alt="Slide 1" />
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className='flex items-center justify-center'>
-                    <Image src="/assets/vjti3.jpg" alt="Slide 1" />
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className='flex items-center justify-center'>
-                    <Image src="/assets/vjti4.jpg" alt="Slide 1" />
-                </div>
-            </SwiperSlide>
-        </Swiper>
+        <div className='px-6 py-6'>
+            <Swiper
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+            >
+                <SwiperSlide>
+                    <div className='flex items-center justify-center'>
+                        <Image src="/assets/vjti1.jpg" alt="Slide 1" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='flex items-center justify-center'>
+                        <Image src="/assets/vjti2.jpg" alt="Slide 1" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='flex items-center justify-center'>
+                        <Image src="/assets/vjti3.jpg" alt="Slide 1" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='flex items-center justify-center'>
+                        <Image src="/assets/vjti4.jpg" alt="Slide 1" />
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+        </div>
     );
 };
 
